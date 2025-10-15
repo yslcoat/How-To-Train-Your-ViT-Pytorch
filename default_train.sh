91dd63fc0a0369f7a51029c98842e02940e15442
YSLCoat <torstein.forseth@outlook.com>
2025-10-15 20:08:45 +0200

    Add default_train.sh

--- default_train.sh
+++ default_train.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 # This is a comment!
-echo Hello World        # This is a comment, too!
+n_epochs=90
+batch_size=256
+model_architecture="lucidrain_vit"
+data_path="/home/yslcoat/data/imagenet1k"
+model_save_dir="/home/yslcoat/trained_models"
+
+systemd-inhibit --what=sleep python train.py \
+    --data "$data_path" \
+    --arch "$model_architecture" \
+    --epochs "$n_epochs" \
+    --batch-size "$batch_size" \
+    --output_parent_dir "$model_save_dir" \
+    --mixup

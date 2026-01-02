5d8011bb2b900b8ed66885f83fb5d8995464e739
YSLCoat <torstein.forseth@outlook.com>
2026-01-02 21:58:54 +0100

    Prettify plots from inspect_history.py

--- inspect_history.py
+++ inspect_history.py
@@ -9,27 +9,31 @@ def main(args):
     print("Loading checkpoint")
     checkpoint = torch.load(args.training_artifact, weights_only=False)
     print("loading compelte")
-    batch_history = checkpoint["batch_history"]
+    # batch_history = checkpoint["batch_history"]
     epoch_history = checkpoint["epoch_history"]
-
-    loss = epoch_history["loss"]
-    batch_loss_avg = batch_history["loss_avg"]
 
     df = pd.DataFrame(epoch_history)
     train_history = df[df["mode"] == "train"]
     val_history = df[df["mode"] == "validate"]
-    # plt.plot(batch_history)
-    # plt.show()
 
-    plt.plot(train_history["loss"], label="Train")
-    plt.plot(val_history["loss"], label="Val")
+    plt.plot(train_history["loss"].to_list(), label="Train") # to_list() to get correct values on the x-axis
+    plt.plot(val_history["loss"].to_list(), label="Val")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Loss Curves")
     plt.legend()
+    plt.grid(True) 
     plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-ta", "--training_artifact", required=True)
+    parser.add_argument(
+        "-ta",
+        "--training_artifact", 
+        required=True, 
+        help="Path to artifact created by train.py, f.ex checkpoint.pth.tar "
+    )
     args = parser.parse_args()
 
     main(args)
